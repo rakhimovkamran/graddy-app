2b40ac4f6b75a3263a081b5f27b06290c5171e6c
Rakhimov Kamran <rakhimovkamran@gmail.com>
2021-12-30 04:11:24 +0500

    | Common | Utils | Add ( Gradient Utils )

---
+++ src/common/utils/index.ts
@@ -0,0 +1,14 @@
+import { IGradient, TColor } from "common/interfaces"
+
+export const createColorStop = (colorStop: TColor) =>
+    ` ${colorStop[0].toUpperCase()} ${colorStop[1]}%`
+
+export const reduceColorStops = (colorStops: TColor[]) => {
+    return colorStops.map((colorStop) => createColorStop(colorStop)).join(", ")
+}
+
+export const generateGradient = (data: IGradient) => {
+    return `linear-gradient(${data.degree}deg, ${reduceColorStops(
+        data.colors
+    )})`
+}
